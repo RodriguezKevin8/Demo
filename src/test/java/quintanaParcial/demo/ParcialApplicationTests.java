1eb459047c3a9fba7f5399dc262a8c9d26faa2db
Kevin Daniel Martínez <140560743+RodriguezKevin8@users.noreply.github.com>
2025-02-24 14:43:12 -0600

    first commit

---
+++ src/test/java/quintanaParcial/demo/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package quintanaParcial.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
